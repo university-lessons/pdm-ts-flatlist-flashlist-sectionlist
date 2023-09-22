696694943cd9723c74b19e751d954211cdd1a173
Andres <andres@pop-os.localdomain>
2023-09-21 21:58:04 -0300

    class code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  FlatList,
+  ScrollView,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { data } from "./MOCK_DATA";
+import { FlashList } from "@shopify/flash-list";
+import { sections } from "./MOCK_DATA_SECTIONS";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Estratégia simples: loop com map */}
+
+      {/* <ScrollView>
+        {data.map((d) => (
+          <Text key={d.id} style={{ height: 100 }}>
+            {d.recipe}
+          </Text>
+        ))}
+      </ScrollView> */}
+
+      {/* Estratégia mais usual: Flatlist */}
+
+      {/* <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Text style={{ height: 100 }}>{item.recipe}</Text>
+        )}
+      /> */}
+
+      {/* Estratégia mais usual: Flashlist (shopify)*/}
+
+      {/* <View style={{ width: "100%", height: "100%" }}>
+        <FlashList
+          data={data}
+          renderItem={({ item }) => (
+            <Text style={{ height: 100 }}>{item.recipe}</Text>
+          )}
+          estimatedItemSize={200}
+          style={{ width: "100%" }}
+        />
+      </View> */}
+
+      <SectionList
+        sections={sections}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.recipe}</Text>
+        )}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+        style={styles.sectionList}
+      />
     </View>
   );
 }
@@ -13,8 +63,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 24,
+  },
+  sectionList: { width: "80%" },
+  item: {
+    borderColor: "grey",
+    borderRadius: 8,
+    borderWidth: 1,
+    height: 100,
+    padding: 8,
+    marginVertical: 4,
+  },
+  sectionHeader: {
+    marginTop: 16,
+    borderTopColor: "grey",
+    borderTopWidth: 2,
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
   },
 });
---
+++ MOCK_DATA_SECTIONS.ts
@@ -0,0 +1,26 @@
+export const sections = [
+  {
+    title: "Massas",
+    data: [
+      { id: 1, recipe: "Macarrão" },
+      { id: 2, recipe: "Canoli" },
+      { id: 3, recipe: "Capeletti" },
+    ],
+  },
+  {
+    title: "Carnes",
+    data: [
+      { id: 1, recipe: "Mortadella" },
+      { id: 2, recipe: "Angus" },
+      { id: 3, recipe: "Paleta" },
+    ],
+  },
+  {
+    title: "Bebidas",
+    data: [
+      { id: 1, recipe: "Suco" },
+      { id: 2, recipe: "Cerveja" },
+      { id: 3, recipe: "Vinho" },
+    ],
+  },
+];
